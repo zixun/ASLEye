2ba5f91bf05df6faf6005753ac3a512c2e4a9efa
zixun <chenyl.exe@gmail.com>
2017-03-20 22:51:38 +0800

    add carthage support

--- ASLEye/Classes/ASLEye.swift
+++ ASLEye/Classes/ASLEye.swift
@@ -11,20 +11,20 @@ import Foundation
 import asl
 
 @objc public protocol ASLEyeDelegate: class {
-    @objc optional func aslEye(aslEye:ASLEye,catchLogs logs:[String])
+    @objc optional func aslEye(_ aslEye:ASLEye,catchLogs logs:[String])
 }
 
-public class ASLEye: NSObject {
+open class ASLEye: NSObject {
     
-    public weak var delegate: ASLEyeDelegate?
+    open weak var delegate: ASLEyeDelegate?
     
-    public var isOpening: Bool {
+    open var isOpening: Bool {
         get {
             return self.timer?.isValid ?? false
         }
     }
     
-    public func open(with interval:TimeInterval) {
+    open func open(with interval:TimeInterval) {
         self.timer = Timer.scheduledTimer(timeInterval: interval,
                                           target: self,
                                           selector: #selector(ASLEye.pollingLogs),
@@ -32,28 +32,28 @@ public class ASLEye: NSObject {
                                           repeats: true)
     }
     
-    public func close() {
+    open func close() {
         self.timer?.invalidate()
         self.timer = nil
         
     }
     
     
-    @objc private func pollingLogs() {
+    @objc fileprivate func pollingLogs() {
         self.queue.async {
             let logs = self.retrieveLogs()
             if logs.count > 0 {
                 DispatchQueue.main.async {
-                    self.delegate?.aslEye?(aslEye: self, catchLogs: logs)
+                    self.delegate?.aslEye?(self, catchLogs: logs)
                 }
             }
         }
     }
     
-    private func retrieveLogs() -> [String] {
+    fileprivate func retrieveLogs() -> [String] {
         var logs = [String]()
         
-        var query: aslmsg = self.initQuery()
+        let query: aslmsg = self.initQuery()
         
         let response: aslresponse? = asl_search(nil, query)
         guard response != nil else {
@@ -73,7 +73,7 @@ public class ASLEye: NSObject {
         return logs
     }
     
-    private func parserLog(from message:aslmsg) ->String {
+    fileprivate func parserLog(from message:aslmsg) ->String {
         let content = asl_get(message, ASL_KEY_MSG)!;
         let msg_id = asl_get(message, ASL_KEY_MSG_ID);
         
@@ -85,8 +85,8 @@ public class ASLEye: NSObject {
         return String(cString: content, encoding: String.Encoding.utf8)!
     }
     
-    private func initQuery() -> aslmsg {
-        var query: aslmsg = asl_new(UInt32(ASL_TYPE_QUERY))
+    fileprivate func initQuery() -> aslmsg {
+        let query: aslmsg = asl_new(UInt32(ASL_TYPE_QUERY))
         //set BundleIdentifier to ASL_KEY_FACILITY
         let bundleIdentifier = (Bundle.main.bundleIdentifier! as NSString).utf8String
         asl_set_query(query, ASL_KEY_FACILITY, bundleIdentifier, UInt32(ASL_QUERY_OP_EQUAL))
@@ -103,10 +103,10 @@ public class ASLEye: NSObject {
         return query
     }
     
-    private var timer: Timer?
+    fileprivate var timer: Timer?
     
-    private var lastMessageID: Int32 = 0
+    fileprivate var lastMessageID: Int32 = 0
     
-    private var queue = DispatchQueue(label: "ASLEyeQueue")
+    fileprivate var queue = DispatchQueue(label: "ASLEyeQueue")
     
 }
